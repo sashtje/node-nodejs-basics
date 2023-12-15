7643dcbdf6e46a084e46858fee4f30a1e26308dd
sashtje <sashtje.lefevre@gmail.com>
2023-12-15 23:22:49 +0300

    feat: add read code

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { join } from 'path';
+import { readFile } from 'fs/promises';
+
+import {getDirName} from "../shared/helpers.js";
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = getDirName(import.meta.url);
+  const fileToReadPath = join(__dirname, 'files', 'fileToRead.txt');
+
+  try {
+    const content = await readFile(fileToReadPath, { encoding: 'utf8' });
+
+    console.log(content);
+  } catch (err) {
+    console.log(err);
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
