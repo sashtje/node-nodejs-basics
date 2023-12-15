a6be95388335ee5fba6bb1faee6358ad2e3c2814
sashtje <sashtje.lefevre@gmail.com>
2023-12-15 22:35:46 +0300

    feat: add rename code

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import { join } from 'path';
+import { rename as renameFile } from 'fs/promises';
+
+import {getDirName, isDirOrFileExists} from "../shared/helpers.js";
+
 const rename = async () => {
-    // Write your code here 
+  const __dirname = getDirName(import.meta.url);
+  const filePath = join(__dirname, 'files', 'wrongFilename.txt');
+  const newFilePath = join(__dirname, 'files', 'properFilename.md');
+
+  if (await isDirOrFileExists(newFilePath)) {
+    throw new Error('FS operation failed');
+  }
+
+  try {
+    await renameFile(filePath, newFilePath);
+  } catch (err) {
+    console.log(err);
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
