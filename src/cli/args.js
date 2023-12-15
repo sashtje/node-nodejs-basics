f4bd83c67b311a6ff469e655a897779c5dc5cc74
sashtje <sashtje.lefevre@gmail.com>
2023-12-15 23:46:46 +0300

    feat: add args code

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = [];
+
+  const argsArr = process.argv.slice(2);
+  for (let i = 1; i < argsArr.length; i += 2) {
+    args.push(`${argsArr[i - 1].slice(2)} is ${argsArr[i]}`);
+  }
+
+  console.log(args.join(', '));
 };
 
 parseArgs();
