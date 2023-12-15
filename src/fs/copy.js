14cb2702496bef858f4fa41a7ba6344fa8d41742
sashtje <sashtje.lefevre@gmail.com>
2023-12-15 21:54:52 +0300

    feat: add simple copy code without copying directories

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import { join } from 'path';
+import { mkdir, readdir, copyFile } from 'fs/promises';
+
+import { getDirName } from '../shared/helpers.js';
+
 const copy = async () => {
-    // Write your code here 
+  const __dirname = getDirName(import.meta.url);
+  const filesDirPath = join(__dirname, 'files');
+  const filesCopyDirPath = join(__dirname, 'files_copy');
+
+  try {
+    await mkdir(filesCopyDirPath);
+    const data = await readdir(filesDirPath);
+
+    for (let file of data) {
+      await copyFile(join(filesDirPath, file), join(filesCopyDirPath, file));
+    }
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await copy();
