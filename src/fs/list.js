7b40641e8e99342f4639587cb096a57ae673fe1a
sashtje <sashtje.lefevre@gmail.com>
2023-12-15 23:17:18 +0300

    feat: add list code

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { join } from 'path';
+import { readdir } from 'fs/promises';
+
+import {getDirName} from "../shared/helpers.js";
+
 const list = async () => {
-    // Write your code here 
+  const __dirname = getDirName(import.meta.url);
+  const filesDirPath = join(__dirname, 'files');
+
+  try {
+    const files = await readdir(filesDirPath, { withFileTypes: true });
+    console.log(files.filter(file => file.isFile()).map(file => file.name));
+  } catch (err) {
+    console.log(err);
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
