8fca578f73aabd8aed9d9754475669247a608730
sashtje <sashtje.lefevre@gmail.com>
2023-12-15 22:55:51 +0300

    feat: add delete code

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import { join } from 'path';
+import { rm } from 'fs/promises';
+
+import {getDirName} from "../shared/helpers.js";
+
 const remove = async () => {
-    // Write your code here 
+  const __dirname = getDirName(import.meta.url);
+  const fileToRemovePath = join(__dirname, 'files', 'fileToRemove.txt');
+
+  try {
+    await rm(fileToRemovePath);
+  } catch (err) {
+    console.log(err);
+    throw new Error('FS operation failed');
+  }
 };
 
 await remove();
