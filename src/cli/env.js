81ff17f082cb45b291618d5c6fa65fdc8cc48916
sashtje <sashtje.lefevre@gmail.com>
2023-12-15 23:35:39 +0300

    feat: add env code

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  const rss_envs = [];
+
+  for (let key in process.env) {
+    if (key.startsWith('RSS_')) {
+      rss_envs.push(`${key}=${process.env[key]}`);
+    }
+  }
+
+  console.log(rss_envs.join('; '));
 };
 
 parseEnv();
