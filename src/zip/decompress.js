df736cfee981e821123c87cb058bbb4b411a2044
sashtje <sashtje.lefevre@gmail.com>
2023-12-17 18:57:53 +0300

    feat: add decompress code

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import { join } from 'node:path';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createUnzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+
+import { getDirName } from '../shared/helpers.js';
+
 const decompress = async () => {
-    // Write your code here 
+  const __dirname = getDirName(import.meta.url);
+  const archivePath = join(__dirname, 'files', 'archive.gz');
+  const filePath = join(__dirname, 'files', 'fileToCompress.txt');
+  const input = createReadStream(archivePath);
+  const unzip = createUnzip();
+  const output = createWriteStream(filePath);
+
+  pipeline(input, unzip, output, err => {
+    if (err) {
+      console.error('An error occurred: ', err);
+    }
+  });
 };
 
 await decompress();
