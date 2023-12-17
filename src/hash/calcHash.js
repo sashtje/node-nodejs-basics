1263e777f97930ec33949251deb2d9100f8e70ab
sashtje <sashtje.lefevre@gmail.com>
2023-12-17 17:35:12 +0300

    feat: add calcHash code

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createReadStream } from 'node:fs';
+import { join } from 'node:path';
+import { createHash } from 'node:crypto';
+import { stdout } from 'node:process';
+
+import { getDirName } from '../shared/helpers.js';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const __dirname = getDirName(import.meta.url);
+    const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    const input = createReadStream(filePath);
+    input.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
