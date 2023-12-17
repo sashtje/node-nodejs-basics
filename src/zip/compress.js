1c597da32bc8b8b12ca303fa2626112fb3bbf099
sashtje <sashtje.lefevre@gmail.com>
2023-12-17 18:40:19 +0300

    feat: add compress code

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import { join } from 'node:path';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+
+import { getDirName } from '../shared/helpers.js';
+
 const compress = async () => {
-    // Write your code here 
+    const __dirname = getDirName(import.meta.url);
+    const fileToCompressPath = join(__dirname, 'files', 'fileToCompress.txt');
+    const archivePath = join(__dirname, 'files', 'archive.gz');
+
+    const input = createReadStream(fileToCompressPath);
+    const gzip = createGzip();
+    const output = createWriteStream(archivePath);
+
+    input.pipe(gzip).pipe(output);
 };
 
 await compress();
