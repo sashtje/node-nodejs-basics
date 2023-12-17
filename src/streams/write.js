cd87e1447392cf807bbc0f48ffadad3a6ac51ed7
sashtje <sashtje.lefevre@gmail.com>
2023-12-17 17:58:21 +0300

    feat: add write code

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { createWriteStream } from 'node:fs';
+import { join } from 'node:path';
+import { stdin } from 'node:process';
+
+import { getDirName } from '../shared/helpers.js';
+
 const write = async () => {
-    // Write your code here 
+  const __dirname = getDirName(import.meta.url);
+  const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+  const output = createWriteStream(filePath);
+
+  stdin.pipe(output);
 };
 
 await write();
