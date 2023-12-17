19f58eaafab933e2e4da7deb801ad3b6f51079c7
sashtje <sashtje.lefevre@gmail.com>
2023-12-17 18:22:26 +0300

    feat: add transform code

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+import { Transform } from 'node:stream';
+import { stdin, stdout } from 'node:process';
+
 const transform = async () => {
-    // Write your code here 
+  const reverse = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, chunk.reverse().slice(1) + '\n');
+    }
+  });
+  stdin.pipe(reverse).pipe(stdout);
 };
 
 await transform();
