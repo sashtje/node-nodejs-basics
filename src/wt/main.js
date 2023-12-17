4e3c4333d700bbc6e96a68a10921200424c368c2
sashtje <sashtje.lefevre@gmail.com>
2023-12-17 20:30:44 +0300

    feat: add workers code

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { cpus } from 'node:os';
+import { join } from 'node:path';
+import { Worker } from 'node:worker_threads';
+
+import { getDirName } from '../shared/helpers.js';
+
 const performCalculations = async () => {
-    // Write your code here
+  const coresNumber = cpus().length;
+  const __dirname = getDirName(import.meta.url);
+  const workerFilePath = join(__dirname, 'worker.js');
+
+  let nStart = 10;
+  let exitThreads = 0;
+
+  const handleExit = () => {
+    exitThreads++;
+
+    if (exitThreads === coresNumber) {
+      console.log(workers);
+    }
+  };
+
+  const workers = (new Array(coresNumber)).fill(undefined).map((_, index) => {
+      const worker = new Worker(workerFilePath, { workerData: nStart++ });
+
+      worker.on('message', (data) => {
+        workers[index] = {status: 'resolved', data};
+      });
+      worker.on('error', () => {
+        workers[index] = {status: 'error', data: null};
+      });
+      worker.on('exit', handleExit);
+
+      return worker;
+    });
 };
 
 await performCalculations();
