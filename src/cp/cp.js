ea2525c3685040c3aaf0681a9e3d24fca469015a
sashtje <sashtje.lefevre@gmail.com>
2023-12-17 23:02:18 +0300

    feat: add child process code

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import { fork } from 'node:child_process';
+import { join } from 'node:path';
+
+import { getDirName } from '../shared/helpers.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __dirname = getDirName(import.meta.url);
+  const processFilePath = join(__dirname, 'files', 'script.js');
+  const childProcess = fork(processFilePath, args);
+
+  childProcess.on('error', (err) => {
+    console.log('Failed to start child process: ', err);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ["hello", "world", "it's", "Christmas"]);
