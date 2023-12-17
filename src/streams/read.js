549c351df5c27f0d4ae13be9e5e530c8ef910b47
sashtje <sashtje.lefevre@gmail.com>
2023-12-17 17:52:11 +0300

    feat: add read code

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from 'node:fs';
+import { join } from 'node:path';
+import { stdout } from 'node:process';
+
+import { getDirName } from '../shared/helpers.js';
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = getDirName(import.meta.url);
+  const filePath = join(__dirname, 'files', 'fileToRead.txt');
+  const input = createReadStream(filePath);
+
+  input.pipe(stdout);
 };
 
 await read();
